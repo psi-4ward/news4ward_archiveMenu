734d21c5a93ecba5c4dd504db0aba39d42db84e8
Christoph Wiechert <wio@psitrax.de>
2013-02-05 19:24:57 +0100

    Added english translation: Many Credits to Jozef Dvorsky

---
+++ languages/en/modules.php
@@ -0,0 +1,17 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * News4ward
+ * a contentelement driven news/blog-system
+ *
+ * @author Christoph Wiechert <wio@psitrax.de>
+ * @copyright 4ward.media GbR <http://www.4wardmedia.de>
+ * @package news4ward_archiveMenu
+ * @filesource
+ * @licence LGPL
+ */
+
+// FE-Modules
+$GLOBALS['TL_LANG']['FMD']['news4wardArchiveMenu']    = array('Archive-Filter', 'Generates a navigation menu to browse the blog archive(s).');
+
+?>
---
+++ languages/en/tl_module.php
@@ -0,0 +1,16 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * News4ward
+ * a contentelement driven news/blog-system
+ *
+ * @author Christoph Wiechert <wio@psitrax.de>
+ * @copyright 4ward.media GbR <http://www.4wardmedia.de>
+ * @package news4ward_archiveMenu
+ * @filesource
+ * @licence LGPL
+ */
+
+// FE-Modules
+$GLOBALS['TL_LANG']['tl_module']['news4ward_archivemenu_type']    = array('Archive format', 'Choose a method for archive grouping.');
+$GLOBALS['TL_LANG']['tl_module']['news4ward_archivemenu_type_reference'] = array('year'=>'year','month'=>'month');
